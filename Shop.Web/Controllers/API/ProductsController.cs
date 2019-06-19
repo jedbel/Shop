03265eb7d80bb2795189bbb4db5220289def07ef
jedbel <jedgara@gmail.com>
2019-06-19 01:22:58 -0500

    API's products controller was added

---
+++ Shop.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Shop.Web.Data;
+
+    /*Este es el controlador de la API(Interfaz que se genera para que otros proyectos la consuma, ej consuman mis productos)
+     El controlador de la API nunca tiene vistas*/
+
+    /*cuando yo publique mi sitio web, a direcciòn que le pongamos + /api/Controller, me va a cceder los datos de este controlador*/
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        /*Como también este controlador necesita los datos de los productos, le vamos a inyectar el ProductsRepository (se le inyecta con el constructor)*/
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+        /*Vamos a crear nuestro método, colo lo llamé GetProducts, por inferencia de métodos Core sabe que es un metodo Get
+         ya que le puse "GetXXXX". Sen embargo por notación se le adiciona el HttpGet. Los productos los retorno envolviendo
+         el resultado en un JSON y eso es lo que devuelve (usando return OK(), crear y sonsu,ir servicios restful es muy sencillo)
+         Pero yo no quiero que me devuelva un OK (un json vacio), sino la lista de productos, para lo cual ya tengo un método
+         creado con anterioridad, */
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+    }
+}
